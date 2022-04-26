117473b2daef7530cfc6f48a03485e6f07549b29
Muklis Dwi <muklis.santoso@gmail.com>
2022-04-26 13:47:19 +0700

    proses login ok

--- main.go
+++ main.go
@@ -6,37 +6,10 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/shopspring/decimal"
 )
-
-type AccountBank struct {
-	IdAccount  int
-	PinAccount int
-	Name       string
-	Balance    decimal.Decimal
-}
-
-var listAccount = []AccountBank{
-	AccountBank{
-		IdAccount:  111122223333,
-		PinAccount: 123456,
-		Name:       "Akun1",
-		Balance:    decimal.Decimal{},
-	},
-	AccountBank{
-		IdAccount:  222233334444,
-		PinAccount: 123456,
-		Name:       "Akun2",
-		Balance:    decimal.Decimal{},
-	},
-	AccountBank{
-		IdAccount:  123412341234,
-		PinAccount: 123456,
-		Name:       "Akun3",
-		Balance:    decimal.Decimal{},
-	},
-}
 
 func main() {
 	StartApp()
@@ -47,13 +20,134 @@ func StartApp() {
 	for {
 		OptionLogin()
 		// fmt.Println(LoginProccess())
+		// CheckError(nil)
 	}
 }
 
+type AccountBank struct {
+	IdAccount  int
+	PinAccount int
+	Name       string
+	Balance    decimal.Decimal
+	History    []HistoryTransaction
+}
+
+type HistoryTransaction struct {
+	Date        time.Time
+	Transaction string
+	Amount      decimal.Decimal
+	Balance     decimal.Decimal
+}
+
+var listAccount = []AccountBank{
+	AccountBank{
+		IdAccount:  111122223333,
+		PinAccount: 123456,
+		Name:       "Akun1",
+		Balance:    decimal.RequireFromString("500000"),
+		History: []HistoryTransaction{
+			HistoryTransaction{
+				Date:        time.Date(2022, 04, 26, 8, 00, 00, 00, time.Local),
+				Transaction: "Setor",
+				Amount:      decimal.RequireFromString("500000"),
+				Balance:     decimal.RequireFromString("500000"),
+			},
+		},
+	},
+	AccountBank{
+		IdAccount:  222233334444,
+		PinAccount: 123456,
+		Name:       "Akun2",
+		Balance:    decimal.RequireFromString("1000000"),
+		History: []HistoryTransaction{
+			HistoryTransaction{
+				Date:        time.Date(2022, 04, 26, 8, 00, 00, 00, time.Local),
+				Transaction: "Setor",
+				Amount:      decimal.RequireFromString("1000000"),
+				Balance:     decimal.RequireFromString("1000000"),
+			},
+		},
+	},
+	AccountBank{
+		IdAccount:  123412341234,
+		PinAccount: 123456,
+		Name:       "Akun3",
+		Balance:    decimal.RequireFromString("1500000"),
+		History: []HistoryTransaction{
+			HistoryTransaction{
+				Date:        time.Date(2022, 04, 26, 8, 00, 00, 00, time.Local),
+				Transaction: "Setor",
+				Amount:      decimal.RequireFromString("1500000"),
+				Balance:     decimal.RequireFromString("1500000"),
+			},
+		},
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// menampilkan menu akun setelah login berhasil
+func OptionAccount(account *AccountBank) {
+	var isLogout bool = false
+	for {
+		fmt.Println("\nSelamat Datang,", account.Name)
+		fmt.Println("1. Tarik Uang")
+		fmt.Println("2. Setor Uang")
+		fmt.Println("3. Transfer")
+		fmt.Println("4. Lihat Riwayat Transaksi")
+		fmt.Println("0. Keluar")
+		fmt.Printf("\nPilih Menu [1]/[2]/[3]/[4]/[0] : ")
+		option, _ := InputScan()
+		switch option {
+		case "0":
+			fmt.Println(account.Name, "telah keluar")
+			fmt.Println("Terima Kasih")
+			isLogout = true
+		case "1":
+			tarikUang()
+		case "2":
+			setorUang()
+		case "3":
+			transferUang()
+		case "4":
+			riwayatTransaksi()
+		default:
+			fmt.Println("\n>>>>>> Pilihan Salah ! <<<<<<")
+		}
+
+		if isLogout {
+			break
+		}
+	}
+}
+
+// fungsi untuk proses tarik uang
+func tarikUang() {
+	fmt.Println("Tarik Uang")
+}
+
+// fungsi untuk proses setor uang
+func setorUang() {
+	fmt.Println("Setor Uang")
+}
+
+// proses transfer uang
+func transferUang() {
+	fmt.Println("Transfer")
+}
+
+// lihat daftar riwayat transaksi
+func riwayatTransaksi() {
+	fmt.Println("Lihar Riwayat Transaksi")
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// menampilkan menu login
 func OptionLogin() {
 	fmt.Println("\n1. Login")
 	fmt.Println("0. Keluar")
-	fmt.Printf("\nPilih Menu [0]/[1] : ")
+	fmt.Printf("\nPilih Menu [1]/[0] : ")
 	option, _ := InputScan()
 	switch option {
 	case "0":
@@ -61,18 +155,19 @@ func OptionLogin() {
 		fmt.Println()
 		os.Exit(0)
 	case "1":
-		isLogin := LoginProccess()
-		if isLogin != nil {
-			fmt.Println(isLogin)
+		isAccount := LoginProccess()
+		if isAccount != nil {
+			OptionAccount(isAccount)
 		} else {
 			fmt.Println("Login Gagal !")
 		}
 	default:
-		fmt.Println("\n>>>>>> Pilihan Salah ! <<<<<<")
+		fmt.Printf("\n>>>>>> Pilihan Salah ! <<<<<<")
 		fmt.Println()
 	}
 }
 
+// melakukan proses login
 func LoginProccess() *AccountBank {
 	fmt.Println("\nSilahakan Login !")
 	fmt.Println("\nMasukan Nomor Rekening :")
@@ -81,12 +176,12 @@ func LoginProccess() *AccountBank {
 	strId, err := InputScan()
 	CheckError(err)
 
-	if cekId, intId := CheckIdAccount(strId); cekId {
+	if cekId, intId := checkIdAccount(strId); cekId {
 		// fmt.Println("\nNomor Rekening Benar !")
 		fmt.Println("\nMasukan PIN :")
 		strPass, err := InputScan()
 		CheckError(err)
-		if cekPin, cekAccount := CheckPassword(strPass, intId); cekPin {
+		if cekPin, cekAccount := checkPassword(strPass, intId); cekPin {
 			account = cekAccount
 		} else {
 			fmt.Println("\nPin Anda Salah !")
@@ -98,7 +193,8 @@ func LoginProccess() *AccountBank {
 	return account
 }
 
-func CheckPassword(strPass string, id int) (bool, *AccountBank) {
+// proses cek password
+func checkPassword(strPass string, id int) (bool, *AccountBank) {
 	var account AccountBank
 	var check bool = false
 	pinAccount, err := strconv.Atoi(strPass)
@@ -114,7 +210,8 @@ func CheckPassword(strPass string, id int) (bool, *AccountBank) {
 	return check, &account
 }
 
-func CheckIdAccount(str string) (bool, int) {
+// proses cek nomor rekening
+func checkIdAccount(str string) (bool, int) {
 	var check bool = false
 	idAccount, err := strconv.Atoi(str)
 	if err != nil {
@@ -129,6 +226,9 @@ func CheckIdAccount(str string) (bool, int) {
 	return check, idAccount
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// fungsi untuk menerima input keyboard
 func InputScan() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
 	str, err := reader.ReadString('\n')
@@ -140,9 +240,12 @@ func InputScan() (string, error) {
 	return str, nil
 }
 
+// fungsi tambahan untuk cek error misal perlu
 func CheckError(err error) {
 	if err != nil {
 		fmt.Println("Error :", err.Error())
 		return
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
